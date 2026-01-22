4ebe77d5bd3eb47f0c9b74a018aa0b2611a926cb
Anas Ali <anasalialmora@gmail.com>
2026-01-22 21:53:47 +0530

    updated latest info (#10)
    Refactor code in next iteration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,24 +1,24 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const themes = require('prism-react-renderer').themes;
+const themes = require("prism-react-renderer").themes;
 const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.oneDark;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Anas Ali.',
-  tagline: 'Software Engineer(UI) at Phonepe',
-  url: 'https://anasali.netlify.app/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/a.png',
+  title: "Anas Ali.",
+  tagline: "Software Engineer(Web) @ Phonepe",
+  url: "https://anasali.netlify.app/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/a.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
   plugins: [
     async function myPlugin(context, options) {
       return {
@@ -37,13 +37,13 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         // docs: {
@@ -58,10 +58,10 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -71,25 +71,25 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        style: 'dark',
-        title: 'Anas Ali',
+        style: "dark",
+        title: "Anas Ali",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/a.png',
+          alt: "My Site Logo",
+          src: "img/a.png",
         },
         hideOnScroll: false,
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'right',
-            label: 'My Shelf',
+            type: "doc",
+            docId: "intro",
+            position: "right",
+            label: "My Shelf",
           },
-          {to: '/blog', label: 'Blog', position: 'right'},
+          { to: "/blog", label: "Blog", position: "right" },
           {
-            href: 'https://github.com/ali-anas',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/ali-anas",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
